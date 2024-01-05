b7b533e94a13a2dad664de3929e847d7626a7f45
ChanMor <morelos.franzchristian@gmail.com>
2024-01-06 00:51:55 +0800

    minor changes in user service

--- api/services/users_service.py
+++ api/services/users_service.py
@@ -10,7 +10,7 @@ def fetch_user_positions(user_id):
     return fetch_data(user_id, "SELECT * FROM positions WHERE user_id = %s")
 
 def fetch_user_portfolio(user_id):
-    return fetch_data(user_id, "SELECT * FROM portfolio WHERE user_id = %s")[0]
+    return fetch_data(user_id, "SELECT * FROM portfolio WHERE user_id = %s")
 
 
 
