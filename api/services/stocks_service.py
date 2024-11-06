f7eeba571b3a8cbb0f5a45cd65dd9ca465f7d4d0
Franz Christian Morelos <110464738+ChanMor@users.noreply.github.com>
2024-11-07 07:19:53 +0800

    Update stocks_service.py

--- api/services/stocks_service.py
+++ api/services/stocks_service.py
@@ -1,27 +1,3 @@
-# from util.stock_data import parse_table
-
-# def listings():
-#     table = parse_table()
-#     stocks = []
-#     for row in table.find_all('tr'):
-#         columns = row.find_all('td')
-#         stock = {'symbol': columns[0].get_text(), 'name': columns[1].get_text(), 'price': columns[2].get_text()}
-#         stocks.append(stock)
-
-#     return stocks
-
-# def fetch_price(symbol):
-#     table = parse_table()
-
-#     for row in table.find_all('tr'):
-#         columns = row.find_all('td')
-
-#         if symbol.upper() == columns[0].get_text():
-#             return { 'price': float(columns[2].get_text().split()[0])}
-
-#     return {'price': None}
-
-
 import requests
 from bs4 import BeautifulSoup
 
